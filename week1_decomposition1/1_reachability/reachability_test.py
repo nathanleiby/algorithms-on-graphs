b3292abc839bdfbf66aab242dac78a5bda7554ce
Nathan Leiby <nathanleiby@gmail.com>
2020-05-03 23:45:55 -0700

    reachability: also gen graphs with no path
    NOTE: In the case of no path where V ~= 1000 and E ~= 1M, the runtime
    gets slow (many seconds), as DFS must search through a lot of cases
    before exhausting all options.

--- week1_decomposition1/1_reachability/reachability_test.py
+++ week1_decomposition1/1_reachability/reachability_test.py
@@ -22,7 +22,7 @@ in2 = """
 out2 = 0
 
 MAX_SIZE = 999
-def gen_graph_with_path(p_edge = 0.5):
+def gen_graph_with_path(p_edge = 0.5, has_path = True):
     num_v = random.randint(2, MAX_SIZE)
     edges = []
 
@@ -34,33 +34,42 @@ def gen_graph_with_path(p_edge = 0.5):
         end = random.randint(1, num_v)
 
     for i in range(1, num_v+1):
+        if not has_path and i == end:
+            # if there's NO path, don't create any edge from 'end'
+            continue
+
         for j in range(i+1, num_v+1):
+            if not has_path and j == end:
+                # if there's NO path, don't create any edge to 'end'
+                continue
+
             if random.random() > p_edge:
                 edges.append( (i,j) )
 
-    # ensure there's a path from start to end
-    len_path = random.randint(1, num_v - 1)
+    if has_path:
+        # ensure there's a path from start to end
+        len_path = random.randint(1, num_v - 1)
 
-    vertices = list(range(1, num_v+1))
-    vertices.remove(end)
-    vertices.remove(start)
-    random.shuffle(vertices)
+        vertices = list(range(1, num_v+1))
+        vertices.remove(end)
+        vertices.remove(start)
+        random.shuffle(vertices)
 
-    cur = start
-    while len_path > 0:
-        if len_path > 1:
-            # if it's an intermediate step, connect something other than the end vertex
-            nxt = vertices.pop()
-        else:
-            # it's the last step, so connect to end
-            nxt = end
-        edges.append( (cur, nxt) )
-        cur = nxt
-        len_path -= 1
+        cur = start
+        while len_path > 0:
+            if len_path > 1:
+                # if it's an intermediate step, connect something other than the end vertex
+                nxt = vertices.pop()
+            else:
+                # it's the last step, so connect to end
+                nxt = end
+            edges.append( (cur, nxt) )
+            cur = nxt
+            len_path -= 1
 
     deduped_edges = list(set(edges))
 
-    return num_v, deduped_edges, start, end
+    return num_v, deduped_edges, start, end, has_path
 
 def graph_to_string(num_v, edges, start, end):
     out = "{} {}\n".format(num_v, len(edges))
@@ -86,13 +95,14 @@ class TestReachability(unittest.TestCase):
     def test_generated_graphs(self):
         for i in range(100):
             p_edge=random.random() # random [0,1]
-            num_v, edges, start, end = gen_graph_with_path(p_edge)
-            print ("graph {:03} (p_edge = {:.02f}, num_v = {:4}, num_e = {})".format(i, p_edge, num_v, len(edges)))
+            has_path = random.choice([True, False])
+            num_v, edges, start, end, _  = gen_graph_with_path(p_edge, has_path)
+            print ("graph {:03} (has_path = {:1}, p_edge = {:.02f}, num_v = {:4}, num_e = {})".format(i, has_path, p_edge, num_v, len(edges)))
             generated_input = graph_to_string(num_v, edges,start, end)
             # print(generated_input)
             adj, x, y = parse_input(generated_input)
             actual = reach(adj, x, y)
-            self.assertEqual(actual, 1)
+            self.assertEqual(actual, has_path)
  
 
 if __name__ == '__main__':
