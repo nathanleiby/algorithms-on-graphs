e8ff39f630fb671cb705de4ba1c427a2a696d5e6
Nathan Leiby <nathanleiby@gmail.com>
2020-05-07 23:20:19 -0700

    add gen graph to acyclic tests

--- week2_decomposition2/1_acyclicity/acyclicity.py
+++ week2_decomposition2/1_acyclicity/acyclicity.py
@@ -1,4 +1,4 @@
-#Uses python3
+# Uses python3
 
 import sys
 
@@ -6,16 +6,19 @@ import sys
 class HasCycleException(Exception):
     pass
 
+
 def acyclic(adj):
     try:
         dfs(adj)
     except HasCycleException as e:
-        print(e)
+        # print(e)
         return 1
     return 0
 
+
 def dfs(adj):
     """ dfs does a depth-first-search """
+    ## TODO: Randomize order that we traverse the nodes
     visited = [False] * len(adj)
     for v in range(len(adj)):
         if visited[v]:
@@ -23,32 +26,37 @@ def dfs(adj):
         explore(adj, visited, v, [v])
 
     # we should have visited every vertex
-    assert(all(visited))
+    assert all(visited)
 
-def explore(adj, visited, v, stack):
+
+def explore(adj, visited, v, ancestors):
     """ explore all vertices that can be reached from v, marking them as visited """
     visited[v] = True
     for neighbor in adj[v]:
-        if neighbor in stack:
+        if neighbor in ancestors:
             # error if we find a back-edge, which indicates a cycle in a DAG
-            raise HasCycleException("Cycle found where {} has back-edge to {}".format(v, neighbor))
+            raise HasCycleException(
+                "Cycle found where {} has back-edge to {}".format(v, neighbor)
+            )
         if not visited[neighbor]:
-            explore(adj, visited, neighbor, stack + [neighbor])
+            explore(adj, visited, neighbor, ancestors + [neighbor])
 
     # we should have visited all neighbors of this vertex
-    assert(neighbor in visited for neighbor in adj[v])
+    assert (neighbor in visited for neighbor in adj[v])
+
 
 def parse_input(text):
     data = list(map(int, text.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
     return adj
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     text = sys.stdin.read()
     adj = parse_input(text)
     print(acyclic(adj))
--- week2_decomposition2/1_acyclicity/acyclicity_test.py
+++ week2_decomposition2/1_acyclicity/acyclicity_test.py
@@ -1,5 +1,7 @@
 from acyclicity import parse_input, acyclic
 import unittest
+import random
+import time
 
 in1 = """
 4 4
@@ -20,10 +22,45 @@ in2 = """
 3 5
 """
 
+MAX_VERTICES = 1000
+
+
+def gen_graph(num_vertices=0, has_cycle=False):
+    if not num_vertices:
+        num_vertices = random.randint(2, MAX_VERTICES)
+
+    adj_list = [None] * num_vertices
+    for i in range(num_vertices):
+        possible_edges = range(i + 1, num_vertices)
+        # only add edges to higher number vertices => no back edges => no cycles
+        num_edges = random.randint(0, num_vertices - i - 1)
+        adj_list[i] = sorted(random.sample(possible_edges, num_edges))
+
+    if has_cycle:
+        back_edge = random.randint(1, num_vertices - 1)
+        adj_list[0] = sorted(adj_list[0] + [back_edge])
+        adj_list[back_edge] = [0] + adj_list[back_edge]
+
+    return adj_list
+
+
 class AcyclicityTest(unittest.TestCase):
     def test_examples(self):
         self.assertEqual(acyclic(parse_input(in1)), 1)
         self.assertEqual(acyclic(parse_input(in2)), 0)
 
+    def test_generated(self):
+        for i in range(100):
+            adj = gen_graph(num_vertices=10 * i)
+            self.assertEqual(acyclic(adj), 0)
+        for i in range(100):
+            adj = gen_graph(num_vertices=10 * i, has_cycle=True)
+            # print(adj)
+            self.assertEqual(acyclic(adj), 1)
+
+
 if __name__ == "__main__":
+    seed = time.time()
+    print("random seed = ", seed)
+    random.seed(seed)
     unittest.main()
